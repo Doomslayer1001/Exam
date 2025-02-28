164c04d3360f30af931d425b55c46201635fcabd
Danush <danushsoosai28@gmail.com>
2025-02-28 10:37:46 -0500

    Question 14 Correction

--- Salary.java
+++ Salary.java
@@ -7,17 +7,22 @@
  */
 public class Salary
 {
-    // instance variables - replace the example below with your own
+    // Question 14
+    //hourly_wage * hours = salary;
+    //    int taxe = 30%
+    //    salary * taxe = taxe_deduction;
+    //    salary-taxe_deduction = total_taxe;
+    //   System.out.println("Total salary is" + total_salary);
     /**
-     * Constructor for objects of class Salary
+     * Question 14(Correction)
      */
-    public int Salary(int hours,int hourly_wage)
+    public double getnetpay(int hours,int hourly_wage)
     {
-        hourly_wage * hours = salary;
-        int taxe = 30%
-        salary * taxe = taxe_deduction;
-        salary-taxe_deduction = total_salary;
+        int salary = hourly_wage * hours;
+        double taxe = 0.3;
+        double taxe_deduction = salary * taxe;
+        double total_salary = salary - taxe_deduction;
         System.out.println("Total salary is" + total_salary);
-        
+        return total_salary;
     }
 }
