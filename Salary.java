8319c1674eada49829114c884f24eb9afb4d5336
Danush <danushsoosai28@gmail.com>
2025-02-28 10:15:56 -0500

    Initial sharing of project

---
+++ Salary.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Salary here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salary
+{
+    // instance variables - replace the example below with your own
+    /**
+     * Constructor for objects of class Salary
+     */
+    public int Salary(int hours,int hourly_wage)
+    {
+        hourly_wage * hours = salary;
+        int taxe = 30%
+        salary * taxe = taxe_deduction;
+        salary-taxe_deduction = total_salary;
+        System.out.println("Total salary is" + total_salary);
+        
+    }
+}
