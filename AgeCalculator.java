d94dd7ce433c48587d6fb747e3a29554d800a4c1
Danush <danushsoosai28@gmail.com>
2025-03-03 09:08:56 -0500

    Age Solution

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -7,15 +7,7 @@
  */
 public class AgeCalculator
 {
-    // instance variables - replace the example below with your own
-    private int currentYear=2025;
-
-    /**
-     * Constructor for objects of class AgeCalculator
-     */
-    public int getAge(int year_of_birth)
-    {
-        year_of_birth = 2004;
+    // year_of_birth = 2004;
         // age = currentYear - year_of_birth;
          // if(age>=16){
          // System.out.println("able to drive");
@@ -25,6 +17,22 @@ public class AgeCalculator
             // //16-age = missing_year;
             // System.out.println("not eligible to drive"+ missing_year);
         // }
-        return 0;
+    private int currentYear=2025;
+
+    /**
+     * Constructor for objects of class AgeCalculator
+     */
+    public int getAge(int year_of_birth)
+    {
+        int age = currentYear - year_of_birth;
+        if(age>=16){
+        System.out.println("able to drive");
+         
+        }
+        else{ 
+            int missing_year = 16-age;
+            System.out.println("not eligible to drive missing" + missing_year + "year left");
+        }
+        return age;
     }
 }
